1bb13570541c3fef39632ab61ea74ae3a093c30f
Ebrahim Emad <ebemad8@gmail.com>
2025-11-25 19:52:51 +0200

    update structure

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 from fastapi import FastAPI
+from dotenv import load_dotenv
+load_dotenv("./config/.env")
+
+from routes import  base
+app=FastAPI()
+app.include_router(base.base_router)
 
 def main():
     print("Hello from document-chat!")
