4b37bf88231207b635d185ea753f7a450f290829
Ebrahim Emad <ebemad8@gmail.com>
2025-12-11 06:31:03 +0200

    adding lru_cache() to make the pbject cahced

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -1,5 +1,5 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
-
+from functools import lru_cache
 class Settings(BaseSettings):
 
     APP_NAME: str
@@ -42,5 +42,6 @@ class Settings(BaseSettings):
     class Config:
         env_file = ".env"
 
+@lru_cache()
 def get_settings():
     return Settings()
