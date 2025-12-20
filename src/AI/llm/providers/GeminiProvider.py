e0d4e3a6187353cc37e946640b69bb87334c475d
Ebrahim Emad <ebemad8@gmail.com>
2025-12-20 16:04:43 +0200

    update gemini config

--- src/AI/llm/providers/GeminiProvider.py
+++ src/AI/llm/providers/GeminiProvider.py
@@ -2,29 +2,33 @@ from ..LLMInterface import LLMInterface
 from ..LLMEnums import GeminiEnums, DocumentTypeEnum
 from google import genai
 from google.genai.types import EmbedContentConfig, GenerateContentConfig
+import logging
 
 
 class GeminiProvider(LLMInterface):
-    def __init__(self,api_key:str,
-                default_max_input_characters: int=1000,
-                default_max_output_tokens: int=1000,
-                default_temperature: float=0.7):
+    def __init__(self,api_key:str,api_url:str,
+                default_input_max_characters: int=1000,
+                default_generation_max_output_tokens: int=1000,
+                default_generation_temperature: float=0.7):
     
         self.api_key = api_key
-        self.client = genai.Client(api_key=self.api_key)
+        self.api_url = api_url
         
-        self.default_max_input_characters = default_max_input_characters
-        self.default_max_output_tokens = default_max_output_tokens
-        self.default_temperature = default_temperature
+        self.default_input_max_characters = default_input_max_characters
+        self.default_generation_max_output_tokens = default_generation_max_output_tokens
+        self.default_generation_temperature = default_generation_temperature
 
         self.generation_model_id = None
+
         self.embedding_model_id = None
         self.embedding_size = None
 
+        self.client=genai.Client(
+            api_key=self.api_key
+        )
 
         self.enums=GeminiEnums
-        self.logger = logging.get_logger(__name__)
-
+        self.logger = logging.getLogger(__name__)
 
     def set_generation_model(self,model_id:str):
         self.generation_model_id=model_id
@@ -32,10 +36,11 @@ class GeminiProvider(LLMInterface):
     def set_embedding_model(self,model_id:str,embedding_size:int):
         self.embedding_model_id=model_id
         self.embedding_size=embedding_size
-    def process_text(self, text: str):
+
+    async def process_text(self, text: str):
         return text[:self.default_input_max_characters].strip()
 
-    def generate_text(self, prompt: str, chat_history: list=[], max_output_tokens: int=None,
+    async def generate_text(self, prompt: str, chat_history: list=[], max_output_tokens: int=None,
                             temperature: float = None):
         if self.client is None:
             self.logger.error("Gemini client is not initialized.")
@@ -47,14 +52,18 @@ class GeminiProvider(LLMInterface):
         
         try:
             config=GenerateContentConfig(
-                system_instruction=prompt,
-                temperature=temperature or self.default_temperature,
-                max_output_tokens=max_output_tokens or self.default_max_output_tokens,
+                temperature=temperature or self.default_generation_temperature,
+                max_output_tokens=max_output_tokens or self.default_generation_max_output_tokens,
             )
-
+            # create chat session
             chat = self.client.aio.chats.create(model=self.generation_model_id)
-            response= chat.send_message(
-                message=chat_history,
+            # add user prompt to history
+            chat_history.append(
+                await self.construct_prompt(prompt=prompt,role=GeminiEnums.USER.value)
+            )
+            # send a message with a full history
+            response= await  chat.send_message(
+                message=prompt,
                 config=config,
             )
 
@@ -67,7 +76,7 @@ class GeminiProvider(LLMInterface):
         except Exception as e:
             self.logger.error(f"Error generating text with Gemini: {str(e)}")
             return None
-    def embed(self, text: str, document_type: str = None):
+    async def embed_text(self, text: str, document_type: str = None):
         if self.client is None:
             self.logger.error("Gemini client is not initialized.")
             return None
@@ -97,10 +106,10 @@ class GeminiProvider(LLMInterface):
         except Exception as e:
             self.logger.error(f"Error embedding text with Gemini: {str(e)}")
             return None
-    def construct_prompt(self, prompt: str, role: str):
+    async def construct_prompt(self, prompt: str, role: str):
         return {
             "role": role,
-            "parts": [self.process_text(prompt)]
+            "parts": [await self.process_text(prompt)]
         }
 
         
